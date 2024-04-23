4f6618e42ae8720df69f14c58331c1c1e825f8ad
noobsiecoder <noobsiecoder@gmail.com>
2024-04-22 23:59:00 -0400

    added bash file

---
+++ bin/run.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILES=('01' '02' '11' '15' '27' '29' '32' '38' '42')
+OUTPUT=$1
+BUILD='Main'
+
+if [ -z "$OUTPUT" ]; then
+    echo "Error: argument not found"
+    echo "ex: /bash/xyz.bash <operation>"
+    echo
+else
+    for file in "${FILES[@]}"; do
+        if [ ! -d "samples/$OUTPUT" ]; then
+            echo "creating directory: $OUTPUT"
+            mkdir -p "samples/$OUTPUT" && 
+                echo "directory created: $OUTPUT" || echo "directory not created: $OUTPUT"
+            echo
+        fi
+        ./build/Main --file=data/drive_00"$file"_sync/image_02/data --output="$OUTPUT"/drive_00"$file"_sync_"$OUTPUT"
+    done
+fi
+
